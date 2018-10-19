f0923f77614e94155b7162105d8b6d5f38a7bcd7
Patrick Mockridge <patrick@engzig.com>
2018-10-19 23:18:40 +0100

    start shell script added

---
+++ catkin_ws/src/udacity_bot/ShellScripts/start_mapping_willow.sh
@@ -0,0 +1,7 @@
+terminator -e "roslaunch udacity_bot world.launch world_file:=~/home/workspace/RoboND-Map-My-World/catkin_ws/src/slam_project/worlds/kitchen_dining.world" &
+sleep 5
+terminator -e "roslaunch udacity_bot teleop.launch" &
+sleep 5
+terminator -e "roslaunch udacity_bot mapping.launch" &
+sleep 5
+terminator -e "roslaunch udacity_bot rviz.launch"

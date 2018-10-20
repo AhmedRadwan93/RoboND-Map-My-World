518fdae466a42db6030ca59ec32d14d898f5c9e5
Patrick Mockridge <patrick@engzig.com>
2018-10-20 21:37:39 +0100

    type correct

--- catkin_ws/src/udacity_bot/ShellScripts/start_mapping_willow.sh
+++ catkin_ws/src/udacity_bot/ShellScripts/start_mapping_willow.sh
@@ -1,4 +1,4 @@
-terminator -e "roslaunch udacity_bot world.launch world_file:=~/home/workspace/RoboND-Map-My-World/catkin_ws/src/slam_project/worlds/willow_garage.world
+terminator -e "roslaunch udacity_bot world.launch world_file:=~/home/workspace/RoboND-Map-My-World/catkin_ws/src/udacity_bot/worlds/willow_garage.world
 .world" &
 sleep 5
 terminator -e "roslaunch udacity_bot teleop.launch" &

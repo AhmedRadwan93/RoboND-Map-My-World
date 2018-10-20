3c1cdd263ea81b39e707f561508d9dbab15ee173
Patrick Mockridge <patrick@engzig.com>
2018-10-20 23:09:13 +0100

    type correct

--- catkin_ws/src/udacity_bot/ShellScripts/start_mapping_kitchen.sh
+++ catkin_ws/src/udacity_bot/ShellScripts/start_mapping_kitchen.sh
@@ -1,4 +1,4 @@
-terminator -e "roslaunch udacity_bot world.launch world_name:=~/home/workspace/RoboND-Map-My-World/catkin_ws/src/slam_project/worlds/kitchen_dining.world" &
+terminator -e "roslaunch udacity_bot kitchen_dining.launch world_name:=~/home/workspace/RoboND-Map-My-World/catkin_ws/src/slam_project/worlds/kitchen_dining.world" &
 sleep 5
 terminator -e "roslaunch udacity_bot teleop.launch" &
 sleep 5

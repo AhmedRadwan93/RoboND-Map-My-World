38338b647f3cbbb9a31471671820cfa4c8cbafd9
Patrick Mockridge <patrick@engzig.com>
2018-10-20 21:53:27 +0100

    type correct

--- catkin_ws/src/udacity_bot/ShellScripts/start_mapping_custom.sh
+++ catkin_ws/src/udacity_bot/ShellScripts/start_mapping_custom.sh
@@ -1,5 +1,5 @@
-terminator -e "roslaunch udacity_bot world.launch world_file:=~/home/workspace/RoboND-Map-My-World/catkin_ws/src/udacity_bot/worlds/willow_garage.world
-.world" &
+terminator -e "roslaunch udacity_bot world.launch world_file:=~/home/workspace/RoboND-Map-My-World/catkin_ws/src/udacity_bot/worlds/custom.world"
+&
 sleep 5
 terminator -e "roslaunch udacity_bot teleop.launch" &
 sleep 5

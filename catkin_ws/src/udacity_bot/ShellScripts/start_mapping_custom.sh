4586cfe719dea8603c1ed948e8f424496734f2c9
Patrick Mockridge <patrick@engzig.com>
2018-10-21 11:48:44 +0100

    New custom map

--- catkin_ws/src/udacity_bot/ShellScripts/start_mapping_custom.sh
+++ catkin_ws/src/udacity_bot/ShellScripts/start_mapping_custom.sh
@@ -1,4 +1,4 @@
-terminator -e "roslaunch udacity_bot custom_world.launch world_file:=~/home/workspace/RoboND-Map-My-World/catkin_ws/src/udacity_bot/worlds/custom.world" &
+terminator -e "roslaunch udacity_bot custom_world.launch world_file:=~/home/workspace/RoboND-Map-My-World/catkin_ws/src/udacity_bot/worlds/random_cafe.world" &
 sleep 5
 terminator -e "roslaunch udacity_bot teleop.launch" &
 sleep 5

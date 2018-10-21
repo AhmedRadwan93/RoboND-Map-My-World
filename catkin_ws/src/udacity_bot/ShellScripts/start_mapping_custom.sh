e70457fd9b6242a5739c721c5e5ecb3dd71bd6dc
Patrick Mockridge <patrick@engzig.com>
2018-10-21 20:24:47 +0100

    updated map to petrol station from cafe. cafe was not working.

--- catkin_ws/src/udacity_bot/ShellScripts/start_mapping_custom.sh
+++ catkin_ws/src/udacity_bot/ShellScripts/start_mapping_custom.sh
@@ -1,4 +1,4 @@
-terminator -e "roslaunch udacity_bot world.launch map:=random_cafe" &
+terminator -e "roslaunch udacity_bot world.launch map:=petrol_station" &
 sleep 5
 terminator -e "roslaunch udacity_bot teleop.launch" &
 sleep 5
